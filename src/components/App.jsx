9e57bb352bcab06a834a240009e4e27446b9d1d2
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-07-20 14:24:12 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,55 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import Statistic from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
 
-const options = ['good', 'neutral', 'bad'];
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  onLeaveFeedback = stat => {
-    this.setState(prev => {
-      return {
-        [stat]: prev[stat] + 1,
-      };
-    });
-  };
-  render() {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const options = ['good', 'neutral', 'bad'];
+  const total = good + neutral + bad;
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Statisctic"/>
-        <Section>
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-          {this.state.good + this.state.neutral + this.state.bad !== 0 ? (
-            <Statistic
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-            />
-          ) : (
-            <Notification title={'There is no feedback'} />
-          )}
-        </Section>
-      </div>
-    );
-  }
+  const onLeaveFeedback = stat => {
+    switch (stat) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Statisctic" />
+      <Section>
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+        {good + neutral + bad !== 0 ? (
+          <Statistic good={good} neutral={neutral} bad={bad} total={total} />
+        ) : (
+          <Notification title={'There is no feedback'} />
+        )}
+      </Section>
+    </div>
+  );
 }
+
 export default App;
